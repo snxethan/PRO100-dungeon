c8abd8f47f09cdf39302cd76600556c2251109eb
Ethan Townsend <ethantownsend73@gmail.com>
2024-08-13 11:58:11 -0600

    Enemy Encounter (FIX)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public LayerMask solidObjectsLayer;
+    public LayerMask EnemyLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -45,6 +46,8 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+        CheckForEnemy();
+
     }
 
     //Needs testing
@@ -55,5 +58,17 @@ public class PlayerController : MonoBehaviour
             return false;
         }
        return true;
+    }
+
+    private void CheckForEnemy()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.05f, EnemyLayer) != null)
+        {
+            if (Random.Range(1, 101) <= 10)
+            {
+                Debug.Log("Enemy Encounter");
+
+            }
+        }
     }
 }
