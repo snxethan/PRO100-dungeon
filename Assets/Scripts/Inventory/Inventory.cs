b71d2e952329806ba54dccca6932e00b4f07165a
JBrincefield <jbrincefield@student.neumont.edu>
2024-08-13 11:26:49 -0600

    ItemSlotUI, GlobalSettings, InventoryUI
    I created the global setting script currently used for color, added the inventory UI script for handiling inventory actions done in the ui screen, added the ItemSlotUI to work with the inventoryUI script

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     [SerializeField] List<ItemSlot> slots;
+
+    public List<ItemSlot> Slots => slots;
+
+    public static Inventory GetInventory()
+    {
+        return FindObjectOfType<PlayerController>().GetComponent<Inventory>();
+    }
 }
 
 [Serializable]
