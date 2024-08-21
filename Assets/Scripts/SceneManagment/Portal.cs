d812013e3003fb0260eeb4f1d56b588a89878df9
JBrincefield <jbrincefield@student.neumont.edu>
2024-08-20 19:05:40 -0600

    finished Scene loading and added indoor scene
    title

--- Assets/Scripts/SceneManagment/Portal.cs
+++ Assets/Scripts/SceneManagment/Portal.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
     [SerializeField] int sceneToLoad = -1;
+    [SerializeField] DestinationIdentifier destinationPortal;
     [SerializeField] Transform spawnPoint;
 
 
@@ -19,19 +20,34 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         
     }
 
+    Fader fader;
+    private void Start()
+    {
+        fader = FindObjectOfType<Fader>();
+
+    }
+
     IEnumerator SwitchScene()
     {
         DontDestroyOnLoad(gameObject);
+
+        yield return fader.FadeIn(0.5f);
         
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
-        var destPortal = FindObjectsOfType<Portal>().First(x => x != this);
+        var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.SetPositionAndSnapToTile(destPortal.spawnPoint.position);
 
+        yield return fader.FadeOut(0.5f);
         player.EndPortalTransition();
         
         Destroy(gameObject);
     }
 
     public Transform SpawnPoint => spawnPoint;
+}
+
+public enum DestinationIdentifier
+{
+    A, B, C, D, E
 }
